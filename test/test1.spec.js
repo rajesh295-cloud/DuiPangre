c2f0cbf686dea131d3c2c760e3e371de562f20e5
Kyser <ssarthak737@gmail.com>
2022-11-11 08:44:43 +0545

    Seller Test

--- test/test1.spec.js
+++ test/test1.spec.js
@@ -1,19 +1,45 @@
 const request = require("supertest");
 const { deleteOne } = require("../models/user");
 const app = require("../routes/userroute")
+const apps = require("../routes/sellerroute")
+
+describe("user registration", () => {
 
 
-it("returns 200 Ok when signup request is valid", () =>{
+    it("returns 200 Ok when signup request is valid", () =>{
 
-    request(app).post('/signup').send({
+        request(app).post('/signup').send({
+    
+            fullname: "Roy",
+            email: "roy@email.com",
+            phonenumber: "9821841218",
+            password: "Passworded",
+        })
+        .then((response)=>{
+            expect(response.status).toBe(200);
+            done();
+        })
+    } )
+})
 
-        fullname: "Roy",
-        email: "roy@email.com",
-        phonenumber: "9821841218",
-        password: "Passworded",
-    })
-    .then((response)=>{
-        expect(response.status).toBe(200);
-        done();
-    })
-} )
+
+
+describe("Seller Registration" ,() =>{
+
+    it("returns 200 Ok when signup request is valid", () =>{
+
+        request(app).post('/seller/signup').send({
+    
+            firstname: "ROy",
+            lastname: "Rolls",
+            email: "roy@email.com",
+            phonenumber: "9821841218",
+            password: "Passworded",
+        })
+        .then((response)=>{
+            expect(response.status).toBe(200);
+            done();
+        })
+    } )
+
+})
