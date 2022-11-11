556844c86ab4508b82d30508b4eb87c401571666
Kyser <ssarthak737@gmail.com>
2022-11-11 08:32:53 +0545

    Tdd test

---
+++ test/test1.spec.js
@@ -0,0 +1,19 @@
+const request = require("supertest");
+const { deleteOne } = require("../models/user");
+const app = require("../routes/userroute")
+
+
+it("returns 200 Ok when signup request is valid", () =>{
+
+    request(app).post('/signup').send({
+
+        fullname: "Roy",
+        email: "roy@email.com",
+        phonenumber: "9821841218",
+        password: "Passworded",
+    })
+    .then((response)=>{
+        expect(response.status).toBe(200);
+        done();
+    })
+} )
