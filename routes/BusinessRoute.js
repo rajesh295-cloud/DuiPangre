cf7807d0f3565c4ff6b44068a672fc5c72228fd1
Kyser <ssarthak737@gmail.com>
2022-11-21 17:49:53 +0545

    Get Profile

--- routes/BusinessRoute.js
+++ routes/BusinessRoute.js
@@ -1,1 +1,3 @@
 const express = require("express")
+
+const Business = require("../models/business")
--- routes/userroute.js
+++ routes/userroute.js
@@ -78,4 +78,16 @@ Router.put("/:id", auth.userGuard, async (req, res) => {
   }
 });
 
+
+
+Router.get("/search/:id", auth.userGuard, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select('-password')
+    if(!user) return res.status(400).json({msg: "User does not exist."})
+
+    res.json(user)
+} catch (err) {
+    return res.status(500).json({msg: err.message})
+}
+});
 module.exports = Router;
