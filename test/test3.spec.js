97b316dfeb91726d76fb4ed59511a0416b599d3e
Kyser <ssarthak737@gmail.com>
2022-11-23 14:52:42 +0545

    Testing  done

--- test/test3.spec.js
+++ test/test3.spec.js
@@ -1,6 +1,8 @@
 
 const request = require("supertest");
 const app = require("../routes/userroute")
+const apps = require("../routes/sellerroute");
+const { expect } = require("chai");
 describe("updating user", () => {
 
     it("returns updated", () =>{
@@ -16,6 +18,64 @@ describe("updating user", () => {
   
 
 })
+
+
+describe("updating seller", () => {
+
+    it("returns updated", () =>{
+        const updateduser = {firstname: "firstname", lastname : "lastnames"}
+        const expectedresponse ={...apps, ...updateduser}
+        request(apps).put('/seller/1').send(updateduser)
+        .end((err, res)=>{
+            expect(res.body).toEqual(expectedresponse)
+            done()
+
+        })
+    } )
+  
+
+})
+
+
+
+describe("get user", () => {
+
+    it("returns updated", () =>{
+        const expectedresponse ={...apps}
+        request(apps).get("/1")
+        .expect(200)
+        .end((err, res) =>{
+            expect(res.body).toEqual(expectedresponse)
+            done()
+        })
+
+    } )
+  
+
+})
+
+
+
+
+describe("update seller", () => {
+
+    it("getting seller", () =>{
+        const expectedresponse ={...app}
+        request(app).get("/seller/1")
+        .expect(200)
+        .end((err, res) =>{
+            expect(res.body).toEqual(expectedresponse)
+            done()
+        })
+
+    } )
+  
+
+})
+
+
+
+
 
 
 
