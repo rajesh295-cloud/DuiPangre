7918ab453824a0e7b21681674b0712845be0370c
Kyser <ssarthak737@gmail.com>
2022-11-24 17:25:41 +0545

    Product Route done

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -18,4 +18,61 @@ Router.post("/add", auth.sellerGuard, upload.single('img'), async(req,res)=>{
  }
 })
 
+
+
+Router.put("/:id", auth.sellerGuard, async(req, res) =>{
+   try{
+      const updatedProduct = await Product.findByIdAndUpdate(
+         req.params.id,
+         {
+            $set: req.body,
+         },
+         {new: true}
+        
+      )
+      res.status(200).json(updatedProduct);
+   }catch(err){
+      res.status(500).json(err)
+   }
+
+})
+
+
+
+Router.delete("/:id", auth.sellerGuard, async(req,res) =>{
+   try{
+      await Product.findByIdAndDelete(req.params.id);
+      res.status(200).json("Product's deleted");
+   }
+   catch(err){
+      res.status(500).json(err);
+   }
+
+
+} )
+
+// Gets all Product
+Router.get("/", async(req, res) =>{
+   try{
+      const product = await Product.find().sort();
+      res.status(200).json(product);
+   }
+   catch(err){
+      res.status(500).json(err)
+   }
+})
+
+// Gets a product
+Router.get("/find/:id", async(req, res) =>{
+   try{
+      const product = await Product.findById(req.params.id);
+      res.status(200).json(product);
+   }
+   catch(err){
+      res.status(500).json(err)
+   }
+
+})
+
+
 module.exports = Router;
