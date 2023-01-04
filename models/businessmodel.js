2cffa0ce79af3bafeb179c032cbc430204592595
Sarthak12397 <64421439+Sarthak12397@users.noreply.github.com>
2023-01-04 19:16:31 +0545

    Update businessmodel.js

--- models/businessmodel.js
+++ models/businessmodel.js
@@ -4,7 +4,6 @@ const businessSchema = new mongoose.Schema(
     {
       name: { type: String, required: true, unique: true },
       slug:{type: String, required:true, unique:true},
-     
       image: { type: String, required: true },
       address: { type: String, required: true , unique:true},
       description: { type: String, required: true , unique:true},
